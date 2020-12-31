8fc13debee44b0c097becd38adb259c3f981e8b2
Nguyen Cao Thiem <1851061743@e.tlu.edu.vn>
2020-12-31 14:24:56 +0700

    Add Login INPUT

---
+++ BTLHQTCSDL2/Dangnhap.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BTLHQTCSDL2
+{
+    partial class Dangnhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dangnhap_submit = new System.Windows.Forms.Button();
+            this.dangnhap_discard = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email người dùng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dangnhap_submit
+            // 
+            this.dangnhap_submit.Location = new System.Drawing.Point(213, 94);
+            this.dangnhap_submit.Name = "dangnhap_submit";
+            this.dangnhap_submit.Size = new System.Drawing.Size(75, 23);
+            this.dangnhap_submit.TabIndex = 4;
+            this.dangnhap_submit.Text = "Đăng nhập";
+            this.dangnhap_submit.UseVisualStyleBackColor = true;
+            // 
+            // dangnhap_discard
+            // 
+            this.dangnhap_discard.Location = new System.Drawing.Point(110, 94);
+            this.dangnhap_discard.Name = "dangnhap_discard";
+            this.dangnhap_discard.Size = new System.Drawing.Size(75, 23);
+            this.dangnhap_discard.TabIndex = 5;
+            this.dangnhap_discard.Text = "Đăng ký";
+            this.dangnhap_discard.UseVisualStyleBackColor = true;
+            // 
+            // Dangnhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 142);
+            this.Controls.Add(this.dangnhap_discard);
+            this.Controls.Add(this.dangnhap_submit);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Dangnhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button dangnhap_submit;
+        private System.Windows.Forms.Button dangnhap_discard;
+    }
+}
